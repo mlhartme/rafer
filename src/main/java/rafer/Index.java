bf427b61066c24ccf5d58f9b7aef232fd9f35a1a
Michael Hartmeier <mlhartme@github.com>
2017-02-05 21:25:54 +0100

    improved index

--- src/main/java/rafer/Index.java
+++ src/main/java/rafer/Index.java
@@ -17,33 +17,43 @@ package rafer;
 
 import net.oneandone.sushi.fs.World;
 import net.oneandone.sushi.fs.file.FileNode;
+import net.oneandone.sushi.util.Diff;
 
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Collections;
 import java.util.List;
 
+/** Creates Index for a backup tree */
 public class Index {
     public static void main(String[] args) throws IOException, ParseException {
         World world;
         FileNode dir;
         List<String> lines;
+        String prev;
+        FileNode idx;
 
         world = World.create();
         dir = world.file("/Volumes/Data/Bilder");
         lines = new ArrayList<>();
         for (FileNode file : dir.find("**/*")) {
-            if (file.isFile()) {
+            if (file.isFile() && !file.getName().startsWith(".")) {
                 try {
                     lines.add(file.getRelative(dir) + " " + file.md5());
                 } catch (IOException e) {
-                    e.printStackTrace();;
+                    e.printStackTrace();
                 }
-            } else {
-                System.out.println(file);
             }
         }
-        dir.join(".index").writeLines(lines);
+        Collections.sort(lines);
+        idx = dir.join(".index");
+        if (idx.exists()) {
+            prev = idx.readString();
+        } else {
+            prev = "";
+        }
+        idx.writeLines(lines);
+        System.out.println(Diff.diff(prev, idx.readString()));
     }
 }

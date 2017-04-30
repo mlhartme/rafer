715cd635d63382789eb4753dab58ac8c15332952
Michael Hartmeier <mlhartme@github.com>
2017-04-30 22:07:21 +0200

    cleanuo

--- src/main/java/rafer/Clean.java
+++ src/main/java/rafer/Clean.java
@@ -17,28 +17,125 @@ package rafer;
 
 import net.oneandone.sushi.fs.World;
 import net.oneandone.sushi.fs.file.FileNode;
+import net.oneandone.sushi.util.Strings;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Clean {
+    private static final SimpleDateFormat FMT = new SimpleDateFormat("yyyy-MM-dd hh.mm.ss");
+    private static final SimpleDateFormat FMT2 = new SimpleDateFormat("yyyy-MM-dd - hh-mm-ss");
+
+    private static final List<String> movies = Strings.toList("AVI", "MOV", "mov", "MP4", "mp4");
+
     public static void main(String[] args) throws IOException, ParseException {
         World world;
         FileNode dir;
+        String name;
+        int idx;
+        String ext;
+        Set<String> extensions = new HashSet<>();
+
+        FMT.parse("2015-09-26 19.26.28");
 
         world = World.create();
+        //dir = world.file("/Volumes/Data/Bilder");
         dir = world.file("/Volumes/Neuerkeller/Bilder");
-        for (FileNode d : dir.find("*/??/??")) {
-            d.checkDirectory();
-            for (FileNode xmp : d.find("*.xmp")) {
-                System.out.println("xmp: " + xmp);
-                xmp.deleteFile();
-            }
-            if (d.list().isEmpty()) {
-                System.out.println("empty: " + d);
-                d.deleteDirectory();
+        int count = 0;
+        for (FileNode month : dir.find("*/??")) {
+            month.checkDirectory();
+            for (FileNode image : month.list()) {
+                count++;
+                name = image.getName();
+                if (name.equals("CaptureOne")) {
+                    continue;
+                }
+                image.checkFile();
+                if (image.getName().equals(".DS_Store")) {
+                    continue;
+                }
+                idx = name.lastIndexOf('.');
+                if (idx == -1) {
+                    throw new IOException(name);
+                }
+                ext = name.substring(idx + 1);
+                extensions.add(ext);
+                name = name.substring(0, idx);
+
+                if (movies.contains(ext)) {
+                    // ok
+                } else if (ext.equals("RAF") && name.startsWith("r") && name.charAt(7) == 'x') {
+                    // ok
+                } else if ("dng".equals(ext) && check(name, "DSCF")) {
+                    // ok
+                } else if ("dng".equals(ext) && check(name, "SAM_")) {
+                    // ok
+                } else if ("JPG".equals(ext) && check(name, "CIMG")) {
+                    // ok
+                } else if ("JPG".equals(ext) && check(name, "SAM_")) {
+                    // ok
+                } else if ("JPG".equals(ext) && check(name, "DSC")) {
+                    // ok
+                } else if ("RAF".equals(ext) && check(name, "DESC")) {
+                    // ok
+                } else if ("JPG".equals(ext) && check(name, "IMG_")) {
+                    // ok
+                } else if (isDate(name)) {
+                } else if (isDate2(name)) {
+                } else {
+                    try {
+                        FMT.parse(name);
+                        // ok
+                    } catch (ParseException e) {
+                        System.out.println("TODO: " + image.getName());
+
+                    }
+                }
             }
         }
+        System.out.println("count: " + count);
+        System.out.println("extensions: " + extensions);
+    }
+
+    private static boolean isDate(String name) {
+        try {
+            FMT.parse(name);
+            return true;
+        } catch (ParseException e) {
+            return false;
+        }
+    }
+
+    private static boolean isDate2(String name) {
+        try {
+            FMT2.parse(name);
+            return true;
+        } catch (ParseException e) {
+            return false;
+        }
+    }
+
+    private static boolean check(String str, String prefix) {
+        if (!str.startsWith(prefix)) {
+            return false;
+        }
+        return digits_4_8(str);
+    }
+
+    private static boolean digits_4_8(String str) {
+        if (str.length() != 8) {
+            return false;
+        }
+        for (int i = 4; i < 8; i++) {
+            if (!Character.isDigit(str.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
